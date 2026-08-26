57ce94a78b6633751eb767cc16bffc0bcba01e73
paivacarlos <paivakk22@gmail.com>
2026-08-26 08:53:05 -0300

    feat: initial commit with Spring Boot 3 scaffold and HelloController

---
+++ src/main/java/com/study/bank_api/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.study.bank_api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Bank API running successfully on Spring Boot 3 & Java 21!";
+    }
+}
